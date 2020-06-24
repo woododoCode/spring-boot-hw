f48bf3ad11924b62116a038e877ce5b8a5db2677
woododoCode <51165001+woododoCode@users.noreply.github.com>
2020-06-24 19:50:37 +0300

    Spring Boot started (#1)
    * Spring Boot started
    
    * Changes in .gitignore

---
+++ src/test/java/dev/hw/springboothw/SpringBootHwApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hw.springboothw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
